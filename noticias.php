e8ec0793962987ad32922d9a872813637921c2b2
lenierrm <lenier.ruiz@gmail.com>
2022-04-16 00:04:36 -0400

    fetch and post
    basic fetch and post creation

--- noticias.php
+++ noticias.php
@@ -113,58 +113,59 @@ function noticias_init(){
             include_once(ABSPATH.WPINC.'/feed.php');
             
             $feed = fetch_feed($feed_url);
-         
-            $limit = $feed->get_item_quantity(6); // specify number of items
-            $items = $feed->get_items(0, $limit); // create an array of items
+            if (!is_wp_error( $feed )){
+                $limit = $feed->get_item_quantity(6); // specify number of items
+                $items = $feed->get_items(0, $limit); // create an array of items
 
-            if ($limit !== 0) {
-                foreach ($items as $item) {
-                    echo $item;
-                    $noticia_post = array(
-                        'post_title'    => $item->get_title(),
-                        'post_content'  => $item->get_description(),
-                        'post_status'   => "publish",
-                        'post_type' => "noticias",
-                    );
-                    $post_id = wp_insert_post($noticia_post, true);
-                    if(false/*$post_id !== 0*/){
-                        $image_url =/* isset($event['cover']['source']) ? $event['cover']['source'] :*/ ''; // Define the image URL here
-                        $image_name  = '';
-                        if (strpos($image_url, '?') !== false) {
-                            $t = explode('?',$image_url);
-                            $image_name = pathinfo(basename($t[0]))['extension'];            
-                        }
-                        $upload_dir = wp_upload_dir(); // Set upload folder
-                        $image_data = file_get_contents($image_url); // Get image data
-                        $unique_file_name = wp_unique_filename( $upload_dir['path'], $image_name ); // Generate unique name
-                        $filename = basename( $unique_file_name );// Create image file name
-                        // Check folder permission and define file location
-                        if( wp_mkdir_p( $upload_dir['path'] ) ) {
-                            $file = $upload_dir['path'] . '/' . $filename;
-                        } else {
-                            $file = $upload_dir['basedir'] . '/' . $filename;
-                        }
-                        // Create the image  file on the server
-                        file_put_contents( $file, $image_data );
-                        // Check image file type
-                        $wp_filetype = wp_check_filetype( $filename, null );
-                        // Set attachment data
-                        $attachment = array(
-                            'post_mime_type' => $wp_filetype['type'],
-                            'post_title'     => sanitize_file_name( $filename ),
-                            'post_content'   => '',
-                            'post_status'    => 'inherit'
+                if ($limit !== 0) {
+                    foreach ($items as $item) {
+                        $imag_url=$item->data['child'][SIMPLEPIE_NAMESPACE_ITUNES]['image'][0]['attribs']['']['href'];
+                        $noticia_post = array(
+                            'post_title'    => $item->get_title(),
+                            'post_content'  => $item->get_description(),
+                            'post_status'   => "publish",
+                            'post_type' => "noticias",
                         );
-                        // Create the attachment
-                        $attach_id = wp_insert_attachment( $attachment, $file, $post_id );
-                        // Include image.php
-                        require_once(ABSPATH . 'wp-admin/includes/image.php');
-                        // Define attachment metadata
-                        $attach_data = wp_generate_attachment_metadata( $attach_id, $file );
-                        // Assign metadata to attachment
-                        wp_update_attachment_metadata( $attach_id, $attach_data );
-                        // And finally assign featured image to post
-                        set_post_thumbnail( $post_id, $attach_id );
+                        $post_id = wp_insert_post($noticia_post, true);
+                        if($post_id !== 0){
+                            $image_url = $imag_url; // Define the image URL here
+                            $image_name  = '';
+                            if (strpos($image_url, '?') !== false) {
+                                $t = explode('?',$image_url);
+                                $image_name = pathinfo(basename($t[0]))['extension'];            
+                            }
+                            $upload_dir = wp_upload_dir(); // Set upload folder
+                            $image_data = file_get_contents($image_url); // Get image data
+                            $unique_file_name = wp_unique_filename( $upload_dir['path'], $image_name ); // Generate unique name
+                            $filename = basename( $unique_file_name );// Create image file name
+                            // Check folder permission and define file location
+                            if( wp_mkdir_p( $upload_dir['path'] ) ) {
+                                $file = $upload_dir['path'] . '/' . $filename;
+                            } else {
+                                $file = $upload_dir['basedir'] . '/' . $filename;
+                            }
+                            // Create the image  file on the server
+                            file_put_contents( $file, $image_data );
+                            // Check image file type
+                            $wp_filetype = wp_check_filetype( $filename, null );
+                            // Set attachment data
+                            $attachment = array(
+                                'post_mime_type' => $wp_filetype['type'],
+                                'post_title'     => sanitize_file_name( $filename ),
+                                'post_content'   => '',
+                                'post_status'    => 'inherit'
+                            );
+                            // Create the attachment
+                            $attach_id = wp_insert_attachment( $attachment, $file, $post_id );
+                            // Include image.php
+                            require_once(ABSPATH . 'wp-admin/includes/image.php');
+                            // Define attachment metadata
+                            $attach_data = wp_generate_attachment_metadata( $attach_id, $file );
+                            // Assign metadata to attachment
+                            wp_update_attachment_metadata( $attach_id, $attach_data );
+                            // And finally assign featured image to post
+                            set_post_thumbnail( $post_id, $attach_id );
+                        }
                     }
                 }
             }         
